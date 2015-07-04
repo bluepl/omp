ed3400597d170693a2358d400ff2b5e123a266b4
Polly <ciaoucho@gmail.com>
2015-07-05 01:58:42 +0800

    Insert Row to Db, Multiple Listview, Audio Capture, Audio Upload, Media Player

---
+++ app/src/main/java/omp.rifeinblu.com.ompmultilevellistviewnavigation.helper/AlertDialogManager.java
@@ -0,0 +1,37 @@
+package omp.rifeinblu.com.ompmultilevellistviewnavigation.helper;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class AlertDialogManager {
+
+    public void showAlertDialog(Context context, String title, String message,
+                                Boolean status) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+        // Setting Dialog Title
+        alertDialog.setTitle(title);
+
+        // Setting Dialog Message
+        alertDialog.setMessage(message);
+
+        if (status != null)
+            // Setting alert dialog icon
+            alertDialog.setIcon((status) ? null : null);
+
+        // Setting OK Button
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+}
---
+++ app/src/main/java/omp.rifeinblu.com.ompmultilevellistviewnavigation.helper/ConnectionDetector.java
@@ -0,0 +1,37 @@
+package omp.rifeinblu.com.ompmultilevellistviewnavigation.helper;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnectionDetector {
+
+    private Context _context;
+
+    public ConnectionDetector(Context context){
+        this._context = context;
+    }
+
+    /**
+     * Checking for all possible internet providers
+     * **/
+    public boolean isConnectingToInternet(){
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null)
+        {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+}
